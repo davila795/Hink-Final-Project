698df183d6ef5824b8dbe7e14f33dda52d756dfe
Carlos Davila <casedaro15@gmail.com>
2021-01-13 02:49:23 +0100

    footer links updated

--- client/src/components/layout/footer/Footer.js
+++ client/src/components/layout/footer/Footer.js
@@ -9,19 +9,19 @@ const Footer = () => {
         <footer>
             <ul className='nav '>
                 <li className='nav-item'>
-                    <Link to='https://www.ironhack.com/es/madrid' target='_blank'>
+                    <a href='https://www.ironhack.com/es/madrid' target='_blank'>
                         <img src={IronhackLogo} alt='ironhack logo' />
-                    </Link>
+                    </a>
                 </li>
                 <li className="nav-item">
-                    <Link to='https://www.linkedin.com/in/carlos-davila-roman/' target='_blank'>
+                    <a href='https://www.linkedin.com/in/carlos-davila-roman/' target='_blank'>
                         <span>Developed by: Carlos Dávila</span>
-                    </Link>
+                    </a>
                 </li>
                 <li className="nav-item">
-                    <Link to='https://github.com/davila795/Hink-Final-Project' target='_blank'>
+                    <a href='https://github.com/davila795/Hink-Final-Project' target='_blank'>
                         <img src={GithubLogo} alt='github logo' />
-                    </Link>
+                    </a>
                 </li>
             </ul>
         </footer>
